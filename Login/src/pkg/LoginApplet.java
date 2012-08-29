48059feae7605e48da389857827a6b663f9eb777
Mark <mark005@pacbell.net>
2012-08-29 15:37:33 -0700

    initialised

---
+++ Login/src/pkg/LoginApplet.java
@@ -0,0 +1,29 @@
+package pkg;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JApplet;
+
+public class LoginApplet extends JApplet implements KeyListener, ActionListener
+{
+
+	@Override
+	public void actionPerformed (ActionEvent arg0)
+	{}
+
+	@Override
+	public void keyPressed (KeyEvent arg0)
+	{}
+
+	@Override
+	public void keyReleased (KeyEvent arg0)
+	{}
+
+	@Override
+	public void keyTyped (KeyEvent arg0)
+	{}
+
+}
